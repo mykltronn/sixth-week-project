54eac852bd834f249ee6a6e3803be6c5acf72607
Michael Ashton <mykl.ashton@gmail.com>
2017-06-29 20:22:51 -0400

    commit before end of day

--- seeders/20170630000252-CreateData.js
+++ seeders/20170630000252-CreateData.js
@@ -2,16 +2,74 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+    return queryInterface.bulkInsert('User', [
+      {
+        username: "Petunia",
+        password: "password",
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+      },
+      {
+        username: "Lavergne",
+        password: "password",
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+      }
+      {
+        username: "Mrs. Pibb",
+        password: "password",
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+      },
+      {
+        username: "Tabitha",
+        password: "password",
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+      },
+      {
+        username: "Henny",
+        password: "password",
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+      },
 
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+  ], {});
+  return queryInterface.bulkInsert('User', [
+    {
+      title: "OMG",
+      body: "Petnuia, you have the most beautiful feathers on your toes!",
+      createdAt: Date.now(),
+      updatedAt: Date.now()
+    },
+    {
+      title: "Squirrels",
+      body: "I don't know if anybody noticed, but the squirrels are back and they've eaten half my breakfast.",
+      createdAt: Date.now(),
+      updatedAt: Date.now()
+    }
+    {
+      title: "",
+      body: "",
+      createdAt: Date.now(),
+      updatedAt: Date.now()
+    },
+    {
+      title: "squirrels again",
+      body: "I was just minding my own business, taking a dirt bath, and that damn squirrel up and stole my dinner!",
+      createdAt: Date.now(),
+      updatedAt: Date.now()
+    },
+    {
+      title: "",
+      body: "",
+      createdAt: Date.now(),
+      updatedAt: Date.now()
+    }
+
+], {});
+
+
   },
 
   down: function (queryInterface, Sequelize) {
